1f3ec595d21c72e4cb67b3b3324b7b79be374eca
rahulsdotnet <32613243+rahulsdotnet@users.noreply.github.com>
2020-08-16 15:16:41 +0530

    Add files via upload
    Added DataAccess And Service Layer file

---
+++ WebDataAccess/EmployeeRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebDataAccess
+{
+    public class EmployeeRepository
+    {
+        SqlConnection connection;
+        public EmployeeRepository()
+        {
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["employeeDbCon"].ConnectionString);
+        }
+
+        public DataTable GetEmployees()
+        {
+            DataTable dt = new DataTable();
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Employee", connection))
+            {
+                SqlDataAdapter sda = new SqlDataAdapter();
+                sda.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                sda.Fill(dataSet);
+                dt = dataSet.Tables[0];
+            }
+            return dt;
+        }
+        public DataTable GetByEmployeeId(int employeeId)
+        {
+            DataTable dt = new DataTable();
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Employee WHERE ID=" + employeeId, connection))
+            {
+                SqlDataAdapter sda = new SqlDataAdapter();
+                sda.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                sda.Fill(dataSet);
+                dt = dataSet.Tables[0];
+            }
+            return dt;
+        }
+    }
+}
---
+++ WebService/EmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using WebDataAccess;
+
+namespace WebService
+{
+    public class EmployeeService
+    {
+        public EmployeeRepository employeeRepository = null;
+        public EmployeeService()
+        {
+            employeeRepository = new EmployeeRepository();
+        }
+        public DataTable GetEmployees()
+        {
+            return employeeRepository.GetEmployees();
+        }
+
+        public DataTable GetByEmployeeId(int employeeId)
+        {
+            return employeeRepository.GetByEmployeeId(employeeId);
+        }
+    }
+}
